3eb959c1f686a6bdff93d11248dd395cf1d09954
Scheyk <kathrin.dostal@gmail.com>
2020-10-19 11:59:23 +0200

    intermediate task 1 finish

--- src/kathi/Intermediate.java
+++ src/kathi/Intermediate.java
@@ -1,4 +1,41 @@
 package kathi;
 
 public class Intermediate {
+
+    public  static void main(String[]args){
+
+        //Create examples that demonstrates the usage of following String methods:
+
+        //1: indexOf(), startsWith(), compareTo(), trim(), replace(), replaceAll() and split()
+        String samuelLIpsum = "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people                                 who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get                                    picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.";
+
+        int theIndexOf = samuelLIpsum.indexOf("t");
+        System.out.println("the index of t is: " + theIndexOf);
+
+        String toCompare = "Some don't, become nothing.";
+        int comparing = samuelLIpsum.compareTo(toCompare);
+        System.out.println(comparing);
+
+        boolean startWith = samuelLIpsum.startsWith("S");
+        System.out.println("start the Ipsum with S? The answer is: " + startWith);
+
+        String toTrim = "   daaaas beeeesteee iiiiiiist-kein     POlizist";
+        System.out.println(toTrim.trim());
+        System.out.println("   daaaas beeeesteee iiiiiiist-kein     POlizist");
+
+        String toReplace = samuelLIpsum.replace("S", "F");
+        System.out.println(toReplace);
+
+        String toReplaceAll = samuelLIpsum.replaceAll("h", "non");
+        System.out.println(toReplaceAll);
+
+        String[] toSplit = samuelLIpsum.split("\\.");
+        String one = toSplit[0];
+        String two = toSplit[1];
+        String three = toSplit[2];
+        System.out.println(three);
+        System.out.println(one);
+        System.out.println(two);
+
+    }
 }
